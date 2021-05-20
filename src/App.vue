adbd91624f02a63b448ffdc28855a1ecf000f79f
Mosquishe <58123296+Mosquishe@users.noreply.github.com>
2021-05-20 20:39:02 +0200

    - Adding information page branch - First setup information page

--- src/App.vue
+++ src/App.vue
@@ -1,103 +1,57 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated class="glossy">
+  <q-layout view="lHh Lpr lFf" class="bg-grey-9 text-grey-1">
+    <q-header elevated class="header">
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          @click="leftDrawerOpen = !leftDrawerOpen"
-          aria-label="Menu"
-          icon="menu"
-        />
-
         <q-toolbar-title>
-          Quasar App
+          Wn8stats
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-tabs class="header__tabs">
+          <q-route-tab to="/" label="Home" exact/>
+          <q-route-tab to="/about" label="Our Mission" />
+          <q-route-tab to="/about" label="Developer" />
+          <q-route-tab to="/about" label="Support Us" />
+          <q-route-tab to="/about" label="About" />
+        </q-tabs>
+        <div>
+        </div>
       </q-toolbar>
+
     </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-2"
-    >
-      <q-list>
-        <q-item-label header>Essential Links</q-item-label>
-        <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="school" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Docs</q-item-label>
-            <q-item-label caption>quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://github.com/quasarframework/">
-          <q-item-section avatar>
-            <q-icon name="code" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Github</q-item-label>
-            <q-item-label caption>github.com/quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://chat.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="chat" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Discord Chat Channel</q-item-label>
-            <q-item-label caption>chat.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://forum.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="forum" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Forum</q-item-label>
-            <q-item-label caption>forum.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://twitter.com/quasarframework">
-          <q-item-section avatar>
-            <q-icon name="rss_feed" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Twitter</q-item-label>
-            <q-item-label caption>@quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-drawer>
-
-    <q-page-container>
-      <HelloWorld />
+    <q-page-container class="layout">
+      <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'LayoutDefault',
 
   components: {
-    HelloWorld
   },
 
   data () {
     return {
-      leftDrawerOpen: false
     }
   }
 }
 </script>
 
-<style>
+<style lang="sass">
+::-webkit-scrollbar
+  width: 7px
+  background: rgba(0, 0, 0, 0.4)
+::-webkit-scrollbar-thumb
+  border-radius: 20px
+  background: rgba(150, 150, 150, 0.6)
+::-webkit-scrollbar-track
+  background: rgba(0, 0, 0, 0.6)
+
+
+.layout
+  width: 90%
+  margin: 0 auto
+.header
+  background-color: rgba(0, 0, 0, 0.6) !important
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,54 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div class="row justify-between content-center content-center">
+      <h4 class="text-weight-light">Ut dapibus dolor a risus luctus ultricies. In finibus eros sed odio posuere tristique. Suspendisse sit amet magna ac urna venenatis pulvinar. Etiam sodales tellus in risus cursus commodo. Aenean vestibulum auctor enim et fermentum. Duis ac felis sit amet lectus vestibulum feugiat eget sed eros.</h4>
+      <img class="image shadow-1" alt="Vue logo" src="https://wn8stats.nl/img/d55fb0117540e47757890cd83100af531b6bb327.jpg">
+    </div>
+    <hr class="inline shadow-1">
+    <div class="row justify-between content-center">
+      <img class="image" alt="Vue logo" src="https://wn8stats.nl/img/d55fb0117540e47757890cd83100af531b6bb327.jpg">
+      <h4 class="text-weight-light text-right">Ut dapibus dolor a risus luctus ultricies. In finibus eros sed odio posuere tristique. Suspendisse sit amet magna ac urna venenatis pulvinar. Etiam sodales tellus in risus cursus commodo. Aenean vestibulum auctor enim et fermentum. Duis ac felis sit amet lectus vestibulum feugiat eget sed eros.</h4>
+    </div>
+    <hr class="inline shadow-1">
+    <div class="row justify-between content-center">
+      <h4 class="text-weight-light">Ut dapibus dolor a risus luctus ultricies. In finibus eros sed odio posuere tristique. Suspendisse sit amet magna ac urna venenatis pulvinar. Etiam sodales tellus in risus cursus commodo. Aenean vestibulum auctor enim et fermentum. Duis ac felis sit amet lectus vestibulum feugiat eget sed eros.</h4>
+      <img class="image" alt="Vue logo" src="https://wn8stats.nl/img/d55fb0117540e47757890cd83100af531b6bb327.jpg">
+    </div>
+    <hr class="inline shadow-1">
+    <div class="row justify-between content-center">
+      <img class="image" alt="Vue logo" src="https://wn8stats.nl/img/d55fb0117540e47757890cd83100af531b6bb327.jpg">
+      <h4 class="text-weight-light text-right">Ut dapibus dolor a risus luctus ultricies. In finibus eros sed odio posuere tristique. Suspendisse sit amet magna ac urna venenatis pulvinar. Etiam sodales tellus in risus cursus commodo. Aenean vestibulum auctor enim et fermentum. Duis ac felis sit amet lectus vestibulum feugiat eget sed eros.</h4>
+    </div>
+    <hr class="inline shadow-1">
+    <div class="row justify-between content-center">
+      <h4 class="text-weight-light">Ut dapibus dolor a risus luctus ultricies. In finibus eros sed odio posuere tristique. Suspendisse sit amet magna ac urna venenatis pulvinar. Etiam sodales tellus in risus cursus commodo. Aenean vestibulum auctor enim et fermentum. Duis ac felis sit amet lectus vestibulum feugiat eget sed eros.</h4>
+      <img class="image" alt="Vue logo" src="https://wn8stats.nl/img/d55fb0117540e47757890cd83100af531b6bb327.jpg">
+    </div>
+    <hr class="inline shadow-1">
+    <div class="row justify-between content-center">
+      <img class="image" alt="Vue logo" src="https://wn8stats.nl/img/d55fb0117540e47757890cd83100af531b6bb327.jpg">
+      <h4 class="text-weight-light text-right">Ut dapibus dolor a risus luctus ultricies. In finibus eros sed odio posuere tristique. Suspendisse sit amet magna ac urna venenatis pulvinar. Etiam sodales tellus in risus cursus commodo. Aenean vestibulum auctor enim et fermentum. Duis ac felis sit amet lectus vestibulum feugiat eget sed eros.</h4>
+    </div>
+    <hr class="inline shadow-1">
+    <div class="row justify-between content-center">
+      <h4 class="text-weight-light">Ut dapibus dolor a risus luctus ultricies. In finibus eros sed odio posuere tristique. Suspendisse sit amet magna ac urna venenatis pulvinar. Etiam sodales tellus in risus cursus commodo. Aenean vestibulum auctor enim et fermentum. Duis ac felis sit amet lectus vestibulum feugiat eget sed eros.</h4>
+      <img class="image" alt="Vue logo" src="https://wn8stats.nl/img/d55fb0117540e47757890cd83100af531b6bb327.jpg">
+    </div>
   </div>
 </template>
+<style lang="sass" scoped>
+hr
+  margin: 25px 0 25px 0
+  height: 1.2px
+  border: none
+  background-color: rgba(180, 180, 180, 0.4)
+h4
+  width: 40%
+  text-shadow: 2px 2px 3px rgba(0, 0, 0, 1)
+.row
+  height: 450px
+.image
+  height: 80%
+  //position: absolute
+  //left: 0
+</style>
